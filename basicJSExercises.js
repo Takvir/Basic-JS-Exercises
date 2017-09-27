35bd50a5ef4dd411d848dd38706281a65661e221
Takvir <takvir.alam@gmail.com>
2017-09-27 09:09:32 +0600

    exercise1,2

--- basicJSExercises.js
+++ basicJSExercises.js
@@ -13,13 +13,13 @@ Output:
  --------------------------- */
 
 function areaTriangle(base, height) {
-  return "No code yet";
+  return 0.5 * base * height;
 }
 console.log("Triangle Area Calculator:");
 /* Uncomment the following to check */
-  // console.log(areaTriangle(2, 7));
-  // console.log(areaTriangle(20, 56.5));
-  // console.log(areaTriangle(50, 34));
+   console.log(areaTriangle(2, 7));
+   console.log(areaTriangle(20, 56.5));
+   console.log(areaTriangle(50, 34));
 
 
 
